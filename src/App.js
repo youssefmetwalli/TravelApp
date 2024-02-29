92efe18398ba87b4be0e48ecb763834fffc84520
youssefmetwalli <ymetwalli22@gmail.com>
2024-02-29 18:31:34 +0900

    added nav bar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
+import NavBar from "./components/NavBar";
 import TopBar from "./components/TopBar";
 
 function App() {
   return (
     <div>
       < TopBar />
+      < NavBar/>
     </div>
   );
 }
