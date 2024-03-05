de1a0eece7f85b40b35b0e6f8aa746602f3b636b
youssefmetwalli <ymetwalli22@gmail.com>
2024-03-05 23:43:14 +0900

    edited layout

--- src/components/Background.jsx
+++ src/components/Background.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Background = () => {
   return (
     <div className='w-full h-[90vh]'>
-        <img src='https://plus.unsplash.com/premium_photo-1664303467567-17891a27998a?q=80&w=1476&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+        <img src='https://wallpapercave.com/wp/wp2887696.jpg'
         alt='/'
         className='w-full h-full object-cover'
         />
