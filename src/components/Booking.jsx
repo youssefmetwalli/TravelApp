e324497d2944025f8578720f4e42308787e0e7aa
youssefmetwalli <ymetwalli22@gmail.com>
2024-04-29 23:22:13 +0900

    edited bookings

--- src/components/Booking.jsx
+++ src/components/Booking.jsx
@@ -5,7 +5,8 @@ const Booking = () => {
     <div id= 'deals' className='max-w-[1140px] m-auto w-full p-4'>
         <form className='lg:flex lg:justify-between w-full items-center'>
             <div className='flex flex-col my-2 py-2'>
-                <label>Destination</label>
+                <label className=''>Destination</label>
+                <label className='text-s font-thin mb-2'>which areas do you want to visit in Egypt?</label>
                 <select name=''id='' className='lg:w-[300px] md:w-full border rounded-md p-2'>
                     <option> Aswan </option>
                     <option> Luxor </option>
@@ -16,18 +17,15 @@ const Booking = () => {
             </div>
             <div className='flex w-full'>
                 <div className='flex flex-col w-full lg:max-w-[250px] my-2 p-2'>
-                    <label> Check In</label>
+                    <label className='mb-2'> From:</label>
                     <input type='date' className='border rounded-md p-2'/>
                 </div>
                 <div className='flex flex-col w-full lg:max-w-[250px] my-2 p-2'>
-                    <label> Check Out</label>
+                    <label className='mb-2'> To:</label>
                     <input type='date' className='border rounded-md p-2'/>
                 </div>
             </div>
-            <div className='flex flex-col my-2 p-2 w-full'>
-                <label> Search </label>
-                <buton className='w-full'> Rates & Availabilities </buton>
-            </div>
+            
         </form>
     </div>
   )
