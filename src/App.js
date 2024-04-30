fd47b1a05ab4ee792cacaba335a1d54813bb2685
youssefmetwalli <ymetwalli22@gmail.com>
2024-04-30 16:51:07 +0900

    updated dependencies

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
 import React from "react";
+import { BrowserRouter as Router } from "react-router-dom";
 import Activities from "./components/Activities";
 import Background from "./components/Background";
 import Booking from "./components/Booking";
 import Contact from "./components/Contact";
 import Footer from "./components/Footer";
+import History from "./pages/History";
 import NavBar from "./components/NavBar";
 import TopBar from "./components/TopBar";
 
 function App() {
   return (
-    <div>
-      {/* < TopBar /> */}
-      < NavBar/>
-      < Background/>
-      < Activities/>
-      < Booking />
-      < Contact />
-      < Footer />
-    </div>
+    <Router>
+      <div>
+        {/* < TopBar /> */}
+        < NavBar />
+        < Background />
+        < Activities />
+        < Booking />
+        < Contact />
+        < Footer />
+        <History />
+      </div>
+    </Router>
   );
 }
 
