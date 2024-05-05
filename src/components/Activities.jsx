716cb9e37fd7e18cf1cba751cdea6d8117e204ab
youssefmetwalli <ymetwalli22@gmail.com>
2024-05-05 18:24:43 +0900

    added navigation

--- src/components/Activities.jsx
+++ src/components/Activities.jsx
@@ -4,20 +4,26 @@ import { Link } from 'react-router-dom';
 const Activities = () => {
   return (
     <div className='max-w-[1140px] m-auto w-full md:flex mt-[-75px]'>
-      <Link to="../pages/History" className='relative p-4 flex flex-col justify-center'>
-        <h3 className='absolute z-10 top-[50%] left-[50%] translate-x-[-50%] text-white text-2xl font-bold'>History & Culture</h3>
-        <img className='w-full h-full object-cover relative border-4 border-white shadow-lg' src='https://wallpapercave.com/wp/wp2887748.jpg' alt='/' />
+      <Link to="/history">
+        <div className='relative p-4 flex flex-col justify-center'>
+          <h3 className='absolute z-10 top-[50%] left-[50%] translate-x-[-50%] text-white text-2xl font-bold'>History & Culture</h3>
+          <img className='w-full h-full object-cover relative border-4 border-white shadow-lg' src='https://wallpapercave.com/wp/wp2887748.jpg' alt='/' />
+        </div>
       </Link>
-      <div className='relative p-4 '>
-        <h3 className='absolute z-10 top-[50%] left-[50%] translate-x-[-50%] text-white text-2xl font-bold'> Liveliness</h3>
-        <img className='w-full h-full object-cover relative border-4 border-white shadow-lg'
-          src='https://wallpapercave.com/wp/wp2888180.jpg' alt='/' />
-      </div>
-      <div className='relative p-4 '>
-        <h3 className='absolute z-10 top-[50%] left-[50%] translate-x-[-50%] text-white text-2xl font-bold'> Natural Beauty</h3>
-        <img className='w-full h-full object-cover relative border-4 border-white shadow-lg'
-          src='https://classic-collection.co.uk/images/blog/wp-content/uploads/2018/01/shutterstock_288797549_1500x1000px.jpg' alt='/' />
-      </div>
+      <Link to="/liveliness">
+        <div className='relative p-4 '>
+          <h3 className='absolute z-10 top-[50%] left-[50%] translate-x-[-50%] text-white text-2xl font-bold'> Liveliness</h3>
+          <img className='w-full h-full object-cover relative border-4 border-white shadow-lg'
+            src='https://wallpapercave.com/wp/wp2888180.jpg' alt='/' />
+        </div>
+      </Link>
+      <Link to="/natural-beauty">
+        <div className='relative p-4 '>
+          <h3 className='absolute z-10 top-[50%] left-[50%] translate-x-[-50%] text-white text-2xl font-bold'> Natural Beauty</h3>
+          <img className='w-full h-full object-cover relative border-4 border-white shadow-lg'
+            src='https://classic-collection.co.uk/images/blog/wp-content/uploads/2018/01/shutterstock_288797549_1500x1000px.jpg' alt='/' />
+        </div>
+      </Link>
 
     </div>
   )
