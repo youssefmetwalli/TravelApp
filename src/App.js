e53e6179bb3397d601a8be663aceec7201c00245
youssefmetwalli <ymetwalli22@gmail.com>
2024-05-05 18:27:02 +0900

    modification

--- src/App.js
+++ src/App.js
@@ -1,27 +1,36 @@
 import React from "react";
-import { BrowserRouter as Router } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Activities from "./components/Activities";
 import Background from "./components/Background";
 import Booking from "./components/Booking";
 import Contact from "./components/Contact";
 import Footer from "./components/Footer";
-import History from "./pages/History";
 import NavBar from "./components/NavBar";
 import TopBar from "./components/TopBar";
+import History from "./pages/History";
+import Liveliness from "./pages/Liveliness";
+import NaturalBeauty from "./pages/NaturalBeauty";
 
 function App() {
   return (
     <Router>
-      <div>
-        {/* < TopBar /> */}
-        < NavBar />
-        < Background />
-        < Activities />
-        < Booking />
-        < Contact />
-        < Footer />
-        <History />
-      </div>
+      < Routes>
+      <Route path='/' element={
+         <div>
+         {/* < TopBar /> */}
+         < NavBar />
+         < Background />
+         <Activities/>   
+         < Booking />
+         < Contact />
+         < Footer />
+       </div>
+         } />
+       <Route path='/history' element={<History/>} />
+         <Route path='/liveliness' element={<Liveliness/>} />
+         <Route path='/natural-beauty' element={<NaturalBeauty/>} />
+         
+        </Routes>
     </Router>
   );
 }
