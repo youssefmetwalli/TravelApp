74731feb3fd177e2dffce8c2869b7d1c1823a3a0
youssefmetwalli <ymetwalli22@gmail.com>
2024-05-05 18:25:45 +0900

    modification

--- src/pages/History.jsx
+++ src/pages/History.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
 
 const History = () => {
-  return (
-    <div>History</div>
-  )
+    return (
+        <div>
+            <div className='flex justify-between w-full min-h-[50px] z-10 bg-gray-700/80 text-white'>
+                History
+            </div>
+            <div className=' bg-red-500 h-full min-h-[50px]'>
+                Content
+            </div>
+        </div>
+    )
 }
 
 export default History
